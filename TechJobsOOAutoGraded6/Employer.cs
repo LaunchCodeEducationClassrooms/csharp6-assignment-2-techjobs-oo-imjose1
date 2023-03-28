123e7a1429e52bb188a4c290733bda68fcf31b1e
Jose g hernandez <jose_ghj@hotmail.com>
2023-03-28 16:01:49 -0500

    task6

--- TechJobsOOAutoGraded6/Employer.cs
+++ TechJobsOOAutoGraded6/Employer.cs
@@ -1,39 +1,22 @@
 ﻿using System;
 namespace TechJobsOOAutoGraded6
 {
-	public class Employer
+	public class Employer :JobField
 	{
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
+        //public new int Id { get; }
+        //private static int nextId = 1;
+        //public new string Value { get; set; }
 
-        public Employer()
-        {
-            Id = nextId;
-            nextId++;
-        }
 
-        public Employer(string value) : this()
+        public Employer(string value) : base(value)
         {
             Value = value;
-
         }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Employer employer &&
-                   Id == employer.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
+       
+      
+        //public abstract override bool Equals(object? obj);
+        //public abstract override int GetHashCode();
+        //public abstract override string ToString();
     }
 }
 
